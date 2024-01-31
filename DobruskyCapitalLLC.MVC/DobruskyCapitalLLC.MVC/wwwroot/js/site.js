09df915538b3e4ecffc61b4a21f16fb0a036d2a4
adobrusky <austindobrusky@gmail.com>
2024-01-30 22:27:16 -0600

    Added content to the about us and indicators sections

--- DobruskyCapitalLLC.MVC/DobruskyCapitalLLC.MVC/wwwroot/js/site.js
+++ DobruskyCapitalLLC.MVC/DobruskyCapitalLLC.MVC/wwwroot/js/site.js
@@ -1,4 +1,9 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const indicators = document.querySelectorAll('.indicator-row');
+    indicators.forEach(ind => {
+        setTimeout(() => {
+            ind.style.opacity = '1';
+            ind.style.transform = 'translateX(0)';
+        }, 200);
+    });
+});
