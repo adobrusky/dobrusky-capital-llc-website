8079b6986e29770ce910d6b22ad0b41d0f5979ab
adobrusky <austindobrusky@gmail.com>
2024-03-04 10:13:48 -0600

    Modified some footer style and cleaned up comments in code.

--- DobruskyCapitalLLC.MVC/DobruskyCapitalLLC.MVC/wwwroot/js/site.js
+++ DobruskyCapitalLLC.MVC/DobruskyCapitalLLC.MVC/wwwroot/js/site.js
@@ -1,25 +1,23 @@
 ﻿$(document).ready(function () {
-    let lastScrollTop = 0; // Keep track of the last scroll position
+    let lastScrollTop = 0;
 
-    // Fade in body
     $('body').css('opacity', 1);
 
-    // Function to check if an element is in the viewport
+    // Check if an element is in the viewport
     function isElementInView($elem) {
         let docViewTop = $(window).scrollTop();
         let docViewBottom = docViewTop + $(window).height();
         let elemTop = $elem.offset().top;
-
-        return ((elemTop <= docViewBottom));
+        return (elemTop <= docViewBottom);
     }
 
-    // Function to animate elements when they enter the viewport
+    // Animate indicator elements when they come into viewport
     function animateElements() {
         $('.indicator-row').each(function (index) {
             let $ind = $(this);
 
             // Calculate a delay based on the element's index to stagger the animations
-            let delay = index * 100; // Adjust the multiplier as needed to change the delay time
+            let delay = index * 100;
 
             // Check if the element is in the viewport and hasn't been animated yet
             if (isElementInView($ind) && !$ind.hasClass('animated')) {
@@ -27,7 +25,7 @@
                     $ind.css({
                         opacity: '1',
                         transform: 'translateX(0)'
-                    }).addClass('animated'); // Add a class to mark it as animated
+                    }).addClass('animated');
                 }, delay);
             }
         });
@@ -39,15 +37,12 @@
 
             let st = $(this).scrollTop() + 1;
             if (st > lastScrollTop) {
-                // Scrolling Down
                 $('.landing-section').css('background-position', `0px ${st * -0.15}px`);
             } else {
-                // Scrolling Up
                 $('.landing-section').css('background-position', `0px ${st * -0.15}px`);
             }
             lastScrollTop = st;
 
-            // Scrolled navbar logic
             if ($(this).scrollTop() > 0 && !$('#navbar').hasClass('.scrolled-navbar')) {
                 $('#navbar').addClass('scrolled-navbar');
             } else {
@@ -60,12 +55,8 @@
         scrollLogic();
     });
 
-    // When the link with href="#about" is clicked
     $('#landing-page a').click(function (e) {
-        // Calculate the position of the start of the #about section
         let aboutPosition = $('#about').offset().top;
-
-        // Get the height of the navbar
         let navbarHeight = $('.navbar').outerHeight();
 
         // Scroll to the #about section, adjusting for the navbar height
